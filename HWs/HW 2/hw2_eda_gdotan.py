247365526a6cd4ee61954fa6ee7de013cff23052
Guy Dotan <gdotan15@gmail.com>
2019-01-30 00:38:30 -0800

    Created HWs folder including HW 1 and HW 2

---
+++ HWs/HW 2/hw2_eda_gdotan.py
@@ -0,0 +1,22 @@
+'''
+     File name: hw2_eda_gdotan.py
+     Author: Guy Dotan
+     Date: 01/27/2019
+     Course: UCLA Stats 404
+     Description: HW #2. Exploratory data analysis on the NBA stats and social media dataset.
+ '''
+
+import pandas as pd
+import glob
+import os
+
+path = 'HW2/social-power-nba/'
+filelist = glob.glob(path + "/*.csv")
+filename = [s.replace('.csv', '').replace(path,'') for s in filelist]
+
+# creates dictionary of dataframes
+dfs = {}
+for x in range(len(filename)):
+    dfs[filename[x]] = pd.read_csv(filelist[x])
+
+
